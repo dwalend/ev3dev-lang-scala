a3518696eec862c5c6c1b0052d0eadff17655953
dwalend <david@walend.net>
2022-03-11 14:07:27 -0500

    Got arc turns working at last

--- CargoConnect/src/net/walend/lessons/GyroArc.scala
+++ CargoConnect/src/net/walend/lessons/GyroArc.scala
@@ -17,7 +17,7 @@ abstract class GyroArc() extends Move:
              goalOdo:MilliMeters,
              goalSpeed: DegreesPerSecond,
              radius:MilliMeters,
-             outerMotor:Motor,
+             arcSpecifier: ArcSpecifier,
              keepGoing: () => Boolean,
              setLeds: () => Unit = Ev3Led.writeBothGreen
   ):Unit = 
@@ -30,116 +30,70 @@ abstract class GyroArc() extends Move:
       //val expectedRemainingDistance = ((goalHeading - heading) * radius / 360).mm
 
       //figure out actual remaining distance
-      val outerMotorDegrees = outerMotor.readPosition() //todo convert this to mm in Robot
-      val remainingDistance = goalOdo - ((outerMotorDegrees * Robot.wheelCircumference)/360).mm
+      val outerMotorPosition = arcSpecifier.outerMotor.readPosition() //todo convert this to mm in Robot
+      val remainingDistance = goalOdo - ((outerMotorPosition * Robot.wheelCircumference)/360).mm
 
-      val expectedRemainingDegrees = ((remainingDistance.value * 360) / (radius.value * 2 * Math.PI.toFloat)).degrees
+      val expectedRemainingDegrees = ((arcSpecifier.turnSign * remainingDistance.value * 360) /
+                                      (radius.value * 2 * Math.PI.toFloat))
+                                      .degrees
       Log.log(s"remainingDegrees $remainingDegrees remainingDistance $remainingDistance expectedRemainingDegrees $expectedRemainingDegrees")
 
       //todo steer adjust 
 
-      val steerAdjust = 
-        if(remainingDegrees == expectedRemainingDegrees) 0.degreesPerSecond
-        else 
-          ((remainingDegrees.abs - expectedRemainingDegrees).value * 6).degreesPerSecond //todo should also be proportional to goal speed
-      val outerMotorSpeed = goalSpeed + steerAdjust
+      val steerAdjust = ((remainingDegrees - expectedRemainingDegrees).value * 6 * arcSpecifier.turnSign).degreesPerSecond //todo should also be proportional to goal speed
+      val outerMotorSpeed: DegreesPerSecond = goalSpeed + steerAdjust
       val innerMotorSpeed = (((goalSpeed - steerAdjust).value * (radius - Robot.wheelToWheel).value)/radius.value).degreesPerSecond
       Log.log(s"gaf steerAdjust is $steerAdjust")
 
       //todo maybe pass in the Robot.drive function and steer adjust ???
       //todo duty cycle instead
-      if(Robot.leftDriveMotor == outerMotor) Robot.drive(outerMotorSpeed,innerMotorSpeed)
-                        else Robot.drive(innerMotorSpeed,outerMotorSpeed)
-      gyroArc(goalHeading,goalOdo,goalSpeed,radius,outerMotor,keepGoing,setLeds)
+      if(Robot.leftDriveMotor == arcSpecifier.outerMotor) Robot.drive(outerMotorSpeed,innerMotorSpeed)
+      else Robot.drive(innerMotorSpeed,outerMotorSpeed)
+      gyroArc(goalHeading,goalOdo,goalSpeed,radius,arcSpecifier,keepGoing,setLeds)
 
   def driveArc(
             goalHeading: Degrees,
             goalSpeed: DegreesPerSecond,
             radius:MilliMeters,
-            outerMotor:Motor,
+            arcSpecifier:ArcSpecifier,
             keepGoing: () => Boolean,
             setLeds: () => Unit = Ev3Led.writeBothGreen
           ): Unit =
     val heading:Degrees = Robot.gyroHeading.readHeading()
     val remainingDegrees = goalHeading - heading
 
-    val outerMotorDegrees = outerMotor.readPosition() //todo convert this to mm in Robot
+    val outerMotorDegrees = arcSpecifier.outerMotor.readPosition() //todo convert this to mm in Robot
     val initialOdo = ((outerMotorDegrees * Robot.wheelCircumference)/360).mm
     val deltaOdo = (goalSpeed.value.sign * (((radius.value * 2 * Math.PI.toFloat) * remainingDegrees.value.abs) /360)).mm
     val goalOdo = initialOdo + deltaOdo
 
     Log.log(s"daf remainingDegrees $remainingDegrees goalOdo $goalOdo")
-    gyroArc(goalHeading,goalOdo,goalSpeed,radius,outerMotor,keepGoing,setLeds)
-
-  @tailrec
-  final def gyroArcBackwards(
-                       goalHeading: Degrees,
-                       goalOdo:MilliMeters,
-                       goalSpeed: DegreesPerSecond,
-                       radius:MilliMeters,
-                       outerMotor:Motor,
-                       keepGoing: () => Boolean,
-                       setLeds: () => Unit = Ev3Led.writeBothGreen
-                     ):Unit =
-    setLeds()
-    if(!keepGoing()) ()
-    else
-      //figure out expected remaining distance
-      val heading:Degrees = Robot.gyroHeading.readHeading()
-      val remainingDegrees = goalHeading - heading
-      //val expectedRemainingDistance = ((goalHeading - heading) * radius / 360).mm
-
-      //figure out actual remaining distance
-      val outerMotorDegrees = outerMotor.readPosition() //todo convert this to mm in Robot
-      val remainingDistance = goalOdo - ((outerMotorDegrees * Robot.wheelCircumference)/360).mm
-
-      //positive for left turns ?
-      //todo get the sign from the intended direction of the turn
-      val expectedRemainingDegrees = (remainingDegrees.sign.value * (remainingDistance.value * 360) / (radius.value * 2 * Math.PI.toFloat)).degrees
-      Log.log(s"grb remainingDegrees $remainingDegrees remainingDistance $remainingDistance expectedRemainingDegrees $expectedRemainingDegrees")
-
-  //todo steer adjust
-
-      val steerAdjust =
-        if(remainingDegrees == expectedRemainingDegrees) 0.degreesPerSecond
-        else
-          //todo steer adjust the other way - use the sign of the speed to make it general
-//          ((remainingDegrees.abs - expectedRemainingDegrees).value * 6).degreesPerSecond //todo should also be proportional to goal speed
-          //abs doesn't seem correct
-          (-(remainingDegrees.abs - expectedRemainingDegrees).value * 6).degreesPerSecond //todo should also be proportional to goal speed
-      val outerMotorSpeed = goalSpeed + steerAdjust
-      val innerMotorSpeed = (((goalSpeed - steerAdjust).value * (radius - Robot.wheelToWheel).value)/radius.value).degreesPerSecond
-      Log.log(s"steerAdjust, outerMotor, innerMotor is $steerAdjust,$outerMotorSpeed,$innerMotorSpeed")
-
-      //todo maybe pass in the Robot.drive function and steer adjust ???
-      //todo duty cycle instead
-      if(Robot.leftDriveMotor == outerMotor) Robot.drive(outerMotorSpeed,innerMotorSpeed)
-      else Robot.drive(innerMotorSpeed,outerMotorSpeed)
-      gyroArcBackwards(goalHeading,goalOdo,goalSpeed,radius,outerMotor,keepGoing,setLeds)
-
-  def driveArcBackwards(
-                goalHeading: Degrees,
-                goalSpeed: DegreesPerSecond,
-                radius:MilliMeters,
-                outerMotor:Motor,
-                keepGoing: () => Boolean,
-                setLeds: () => Unit = Ev3Led.writeBothGreen
-              ): Unit =
-    val heading:Degrees = Robot.gyroHeading.readHeading()
-    val remainingDegrees = goalHeading - heading
-
-    val outerMotorDegrees = outerMotor.readPosition() //todo convert this to mm in Robot
-    val initialOdo = ((outerMotorDegrees * Robot.wheelCircumference)/360).mm
-    val deltaOdo = (goalSpeed.value.sign * (((radius.value * 2 * Math.PI.toFloat) * remainingDegrees.value.abs) /360)).mm
-    val goalOdo = initialOdo + deltaOdo
-
-    Log.log(s"drb remainingDegrees $remainingDegrees goalOdo $goalOdo")
-    gyroArcBackwards(goalHeading,goalOdo,goalSpeed,radius,outerMotor,keepGoing,setLeds)
-
+    gyroArc(goalHeading,goalOdo,goalSpeed,radius,arcSpecifier,keepGoing,setLeds)
 
   def notRightEnough(goalHeading:Degrees):Boolean = Robot.gyroHeading.readHeading() < goalHeading
 
   def notLeftEnough(goalHeading:Degrees):Boolean = Robot.gyroHeading.readHeading() > goalHeading
+
+  sealed trait ArcSpecifier:
+    def outerMotor:Motor
+    def turnSign:Int
+
+  object ForwardRight extends ArcSpecifier:
+    override val outerMotor:Motor = Robot.leftDriveMotor
+    override val turnSign: Int = 1
+
+  object ForwardLeft extends ArcSpecifier:
+    override val outerMotor:Motor = Robot.rightDriveMotor
+    override val turnSign: Int = -1
+
+  object BackwardRight extends ArcSpecifier:
+    override val outerMotor:Motor = Robot.rightDriveMotor
+    override val turnSign: Int = -1
+
+  object BackwardLeft extends ArcSpecifier:
+    override val outerMotor:Motor = Robot.leftDriveMotor
+    override val turnSign: Int = 1
+
 
 case class GyroArcForwardRight(
             goalHeading: Degrees,
@@ -154,7 +108,7 @@ case class GyroArcForwardRight(
 //todo what to do for "not far enough" ? distance and/or angle?
     def notFarEnough():Boolean = notRightEnough(goalHeading)
 
-    driveArc(goalHeading,goalSpeed,radius,outerMotor,notFarEnough,setLeds)
+    driveArc(goalHeading,goalSpeed,radius,ForwardRight,notFarEnough,setLeds)
 
 case class GyroArcForwardLeft(
                                 goalHeading: Degrees,
@@ -168,7 +122,7 @@ case class GyroArcForwardLeft(
     //todo what to do for "not far enough" ? distance and/or angle?
     def notFarEnough():Boolean = notLeftEnough(goalHeading)
 
-    driveArc(goalHeading,goalSpeed,radius,outerMotor,notFarEnough,setLeds)
+    driveArc(goalHeading,goalSpeed,radius,ForwardLeft,notFarEnough,setLeds)
 
 case class GyroArcBackwardRight(
                                 goalHeading: Degrees,
@@ -183,7 +137,7 @@ case class GyroArcBackwardRight(
     //todo what to do for "not far enough" ? distance and/or angle?
     def notFarEnough():Boolean = notRightEnough(goalHeading)
 
-    driveArcBackwards(goalHeading,goalSpeed,radius,outerMotor,notFarEnough,setLeds)
+    driveArc(goalHeading,goalSpeed,radius,BackwardRight,notFarEnough,setLeds)
 
 case class GyroArcBackwardLeft(
                                goalHeading: Degrees,
@@ -197,7 +151,7 @@ case class GyroArcBackwardLeft(
     //todo what to do for "not far enough" ? distance and/or angle?
     def notFarEnough():Boolean = notLeftEnough(goalHeading)
 
-    driveArcBackwards(goalHeading,goalSpeed,radius,outerMotor,notFarEnough,setLeds)
+    driveArc(goalHeading,goalSpeed,radius,BackwardLeft,notFarEnough,setLeds)
 
 object TestGyroArc extends Runnable:
   val actions: Array[TtyMenuAction] = Array(
