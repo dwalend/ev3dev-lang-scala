052dcd65802d980a93b5a34de3416c81a912783b
dwalend <david@walend.net>
2022-08-12 13:58:25 -0400

    Refined the bash script to work from the ev3dev menu

--- SuperPowered/SuperPowered.bash
+++ SuperPowered/SuperPowered.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
-#Command line for getting the cargo connect jar up and running
-brickrun -r -- java -cp Ev3LangScala.jar ev3dev4s.JarRunner SuperPowered.jar superpowered.Starter
+#Command line for getting the superpowered jar up and running
+java -cp Ev3LangScala.jar ev3dev4s.JarRunner SuperPowered.jar superpowered.Starter
+
+# In a shell use
+# brickrun -r -- java -cp Ev3LangScala.jar ev3dev4s.JarRunner SuperPowered.jar superpowered.Starter
