0abc485c2cf665bb8e1f04beb1b4577859850c32
dwalend <david@walend.net>
2022-09-19 15:12:23 -0400

    Added a lot of ways to move to HelloWorld.

--- SuperPowered/src/superpowered/HelloWorld.scala
+++ SuperPowered/src/superpowered/HelloWorld.scala
@@ -1,32 +1,97 @@
 package superpowered
 
 import ev3dev4s.Log
-import ev3dev4s.lego.{Display, Sound}
+import ev3dev4s.actuators.MotorPort
+import ev3dev4s.lego.{Display, Movement, Sound}
 import ev3dev4s.measure.Conversions.IntConversions
 import ev3dev4s.sensors.Ev3KeyPad
 
 import java.lang.Runnable
-import scala.Unit
-import scala.StringContext
+import scala.{None, Option, StringContext, Unit}
 
 /**
  * A HelloWorld for the Ev3 that demonstrates five ways to communicate with the technicians.
  */
 object HelloWorld extends Runnable {
   override def run(): Unit = {
-    Sound.playBeep(220.Hz,200.ms)
-    Display.write("Hello World!",0)
-    Display.setLedsTo(Display.LedColor.Green)
-    Sound.speak("Hello World")
-    Display.write("Push Button",1)
+    Sound.playBeep(220.Hz, 200.ms)
 
-    while(Ev3KeyPad.blockUntilAnyKey()._2 != Ev3KeyPad.State.Released) {
-      //don't do anything
-    }
+    Display.write("Hello World!", 0)
+    Display.write("Push Button", 1)
+
+    Movement.setMovementMotorsTo(MotorPort.A,MotorPort.C)
+
+    //todo try making functions for these and stringing them together
+    //todo come up with a picture for each
+//Forward
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = 500.degreesPerSecond,
+      rightSpeed = 500.degreesPerSecond
+    )
+//Backward
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = -500.degreesPerSecond,
+      rightSpeed = -500.degreesPerSecond
+    )
+
+    //Rotate Left
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = -500.degreesPerSecond,
+      rightSpeed = 500.degreesPerSecond
+    )
+
+    //Rotate Right
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = 500.degreesPerSecond,
+      rightSpeed = -500.degreesPerSecond
+    )
+
+    //Pivot Left Forward
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = 500.degreesPerSecond,
+      rightSpeed = 0.degreesPerSecond
+    )
+
+    //Pivot Left Backward
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = -500.degreesPerSecond,
+      rightSpeed = 0.degreesPerSecond
+    )
+
+    //Pivot Right Forward
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = 500.degreesPerSecond,
+      rightSpeed = 0.degreesPerSecond
+    )
+
+    //Pivot Right Backward
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = 0.degreesPerSecond,
+      rightSpeed = -500.degreesPerSecond
+    )
+
+    //Drive a Curve
+    Movement.move(
+      motorDegrees = 900.degrees,
+      leftSpeed = 200.degreesPerSecond,
+      rightSpeed = 500.degreesPerSecond)
+
     Log.log("Button pushed")
-    Sound.speak("Thank you")
+    Sound.speak("cat no cat no cat")
 
     Display.clearLcd()
     Display.setLedsTo(Display.LedColor.Off)
+    while (Ev3KeyPad.blockUntilAnyKey()._2 != Ev3KeyPad.State.Released) {
+      //don't do anything
+    }
+
   }
 }
