4b1f824c1c128e59d760e7a2306e05446fdd3012
dwalend <david@walend.net>
2022-10-15 21:13:03 -0400

    most of the tirns are prfick. you cant do the tirn to get the badery for the toy dinowsor bekus of the atachmet. id recomend sortining the move forom the wind mill a litl bit.
    by: Caroline Rose Walend

--- SuperPowered/src/superpowered/WindmillTrip.scala
+++ SuperPowered/src/superpowered/WindmillTrip.scala
@@ -35,17 +35,52 @@ object WindmillTrip extends Runnable{
 
   override def run(): Unit = {
 
+     // This is form start to TV
+
     Gyroscope.reset(SensorPort.One)
 
     Robot.movestraight(500.mm, 200.degreesPerSecond)
 
     Robot.movestraight(-50.mm,speed = -200.degreesPerSecond)
 
+    // frome TV to wind mill
+
     Robot.leftRotation(-45.degrees)
 
     Robot.movestraight(380.mm,speed=200.degreesPerSecond)
 
     Robot.rightRotation(45.degrees)
+
+    // do Wind mil task
+
+   Robot.movestraight(200.mm,speed=400.degreesPerSecond)
+
+    Robot.movestraight(-40.mm, speed = -400.degreesPerSecond)
+
+    Robot.movestraight(40.mm, speed = 400.degreesPerSecond)
+
+  Robot.movestraight(-40.mm, speed = -400.degreesPerSecond)
+
+Robot.movestraight(40.mm, speed = 400.degreesPerSecond)
+
+Robot.movestraight(-40.mm, speed = -400.degreesPerSecond)
+
+    Robot.movestraight(40.mm, speed = 400.degreesPerSecond)
+
+    Robot.movestraight(-40.mm, speed = -400.degreesPerSecond)
+
+    Robot.movestraight(40.mm, speed = 400.degreesPerSecond)
+
+    Robot.movestraight(-80.mm, speed = -400.degreesPerSecond)
+
+  //-90
+   Robot.leftRotation(-90.degrees)
+
+     Robot.movestraight(190.mm, speed = 200.degreesPerSecond)
+
+    Robot.leftRotation(-225.degrees)
+
+    Robot.movestraight(800.mm, speed = 700.degreesPerSecond)
 
 
   }
