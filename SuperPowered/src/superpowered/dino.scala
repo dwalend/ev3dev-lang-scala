5a65f540ed51a523bda3ace154e4429117fc529a
dwalend <david@walend.net>
2022-10-19 17:16:23 -0400

    Caroline and Isabelle move the dinosaur

---
+++ SuperPowered/src/superpowered/dino.scala
@@ -0,0 +1,16 @@
+package superpowered
+
+import ev3dev4s.measure.Conversions.IntConversions
+
+import java.lang.Runnable
+import scala.Unit
+
+object dino extends Runnable{
+
+  def run(): Unit = {
+
+    Robot.movestraight(1840.mm, 400.degreesPerSecond)
+
+  }
+}
+//1340
