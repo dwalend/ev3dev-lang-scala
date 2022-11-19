44ebf31398a497df6b9a604ca04518ff6b71849f
dwalend <david@walend.net>
2022-11-19 13:13:41 -0500

    Commit J's menu work so she can continue when Avril claims this laptop

--- SuperPowered/src/superpowered/Menu.scala
+++ SuperPowered/src/superpowered/Menu.scala
@@ -1,145 +1,71 @@
 package superpowered
 
-import ev3dev4s.actuators.Ev3Led
 import ev3dev4s.Log
-import ev3dev4s.lcd.tty.Lcd
+import ev3dev4s.lego.{Display, Sound}
 import ev3dev4s.sensors.Ev3KeyPad
 
-import java.lang.{Runnable, System}
+import java.lang.Runnable
 import scala.Predef._
-import scala.{Array, Boolean, Int, Seq, Unit, volatile}
+import scala.{Array, Boolean, Int, List, Seq, Unit, volatile}
+import scala.StringContext
 
 /**
- * Copied from Ev3LangScalaExample as a starting point.
  *
- * @author
+ *
+ * @author David Walend
  * @since v0.0.0
  */
 object Menu extends Runnable {
 
-  val actions: Seq[Runnable] = Seq(
-    Green,
-    Yellow,
-    Red,
-    Off,
-    Reload
+  val trips: List[Runnable] = List(
+    WindmillTrip,
+    dino,
+    LastWorld
   )
 
-  @volatile var selectedAction: Runnable = actions.head
-  @volatile var keepGoing: Boolean = true
+  var currentTrip = trips.head
 
   override def run(): Unit = {
-    drawScreen()
-    Green.run()
+    showTrip()
+    waitForKey() match {
 
-    Log.log("start menu loop")
-    while (keepGoing) {
-      val key: (Ev3KeyPad.Key, Ev3KeyPad.State) = Ev3KeyPad.blockUntilAnyKey()
-      key match {
-        case (Ev3KeyPad.Key.Enter, Ev3KeyPad.State.Released) => doAction()
-        case (Ev3KeyPad.Key.Right, Ev3KeyPad.State.Released) => incrementSelected()
-        case (Ev3KeyPad.Key.Left, Ev3KeyPad.State.Released) => decrementSelected()
-        case (Ev3KeyPad.Key.Escape, Ev3KeyPad.State.Released) => stopLoop()
-        case _ => //do nothing
-      }
-    }
-    Log.log("end menu loop")
-  } //run
+      case Ev3KeyPad.Key.Enter => currentTrip.run()
+      case Ev3KeyPad.Key.Down => nextTrip()
+      case Ev3KeyPad.Key.Up => previousTrip()
+      case _ => Log.log(s"No key??")
 
-  def doAction(): Unit = {
-    selectedAction.run()
-    System.gc()
-    drawScreen()
-  }
-
-
-  def stopLoop(): Unit = {
-    keepGoing = false
-  }
-
-  def incrementSelected(): Unit = {
-    selectedAction = actions.span(_ != selectedAction)._2.tail.headOption.getOrElse(actions.head)
-    drawScreen()
-  }
-
-  def decrementSelected(): Unit = {
-    selectedAction = actions.span(_ != selectedAction)._1.lastOption.getOrElse(actions.last)
-    drawScreen()
-  }
-
-  def drawScreen(): Unit = {
-    Lcd.clear()
-    drawActionWidget(3)
-    Lcd.flush()
-  }
-
-  def drawActionWidget(row: Int): Unit = {
-    val label = selectedAction match {
-      case menuAction: MenuAction => menuAction.label
-      case runnable: Runnable => runnable.getClass.getSimpleName.dropRight(1)
     }
 
-    Lcd.set(row, label, Lcd.CENTER)
-    Lcd.set(row, 0, '<')
-    Lcd.set(row, 10, '>')
+
+  }
+  def nextTrip():Unit = {
+    currentTrip = trips.span(_ != currentTrip)
+      ._2 .tail.headOption.getOrElse(trips.head)
+    showTrip()
   }
 
-  object Reload extends MenuAction {
-    val label = "Reload"
+   def previousTrip():Unit = {
+     currentTrip = trips.span(_ != currentTrip)
+       ._1 .lastOption.getOrElse(trips.last)
+     showTrip()
+   }
 
-    override def run(): Unit = stopLoop()
+  def showTrip() = {
+    Display.clearLcd()
+    Display.write(currentTrip.getClass.getSimpleName, row=2)
   }
 
-  def main(args: Array[String]): Unit = {
-    run()
-  }
-}
+  def waitForKey():Ev3KeyPad.Key = {
+    var key: Ev3KeyPad.Key = null
 
-trait MenuAction extends Runnable {
-  /**
-   * @return 9-or-fewer characters as the label for this menu action
-   */
-  def label: String
-}
+    while ({
+      val keyPressed: (Ev3KeyPad.Key, Ev3KeyPad.State) = Ev3KeyPad.blockUntilAnyKey()
+      key = keyPressed._1
 
-object Green extends MenuAction {
-  val label = "Green"
-
-  override def run(): Unit = {
-    Ev3Led.Left.writeOff()
-    Ev3Led.Right.writeOff()
-    Ev3Led.Left.writeGreen()
-    Ev3Led.Right.writeGreen()
-  }
-}
-
-object Red extends MenuAction {
-  val label = "Red"
-
-  override def run(): Unit = {
-    Ev3Led.Left.writeOff()
-    Ev3Led.Right.writeOff()
-    Ev3Led.Left.writeRed()
-    Ev3Led.Right.writeRed()
-  }
-}
-
-object Yellow extends MenuAction {
-  val label = "Yellow"
-
-  override def run(): Unit = {
-    Ev3Led.Left.writeOff()
-    Ev3Led.Right.writeOff()
-    Ev3Led.Left.writeYellow()
-    Ev3Led.Right.writeYellow()
-  }
-}
-
-object Off extends MenuAction {
-  val label = "Off"
-
-  override def run(): Unit = {
-    Ev3Led.Left.writeOff()
-    Ev3Led.Right.writeOff()
+      Ev3KeyPad.blockUntilAnyKey()._2 != Ev3KeyPad.State.Released}){
+      //don't do anything
+    }
+    Log.log(s"key is $key")
+    key
   }
 }
