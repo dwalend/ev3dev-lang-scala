c945f88bd2e9311098cc342da443665f96f3a267
dwalend <david@walend.net>
2023-09-29 23:02:15 -0400

    Aanya's simpler route plus some pokemon inspired poetry

--- MasterPiece/src/masterpiece/SceneChangeTrip.scala
+++ MasterPiece/src/masterpiece/SceneChangeTrip.scala
@@ -11,16 +11,22 @@ import scala.Unit
 object SceneChangeTrip extends Runnable{
   //start with back of robot on very back wall - zero there.
   override def run(): Unit = {
+    //Set the gyroscope
     Gyroscope.reset(SensorPort.One)
-    Gyroscope.setHeading(SensorPort.One, 45.degrees)
-    Robot.movestraight(320.mm, 245.degreesPerSecond)
+    Gyroscope.setHeading(SensorPort.One, 0.degrees)
 
-    Robot.leftRotation(0.degrees)
-    Robot.movestraight(320.mm, 245.degreesPerSecond)
+    //move from start to the scene changer
 
-
-
+    Robot.movestraight(675.mm, 245.degreesPerSecond)
     Robot.leftRotation(-45.degrees)
+
+
+    /*
+    Robot.rightRotation(45.degrees)
+    Robot.movestraight(320.mm, 245.degreesPerSecond)
+    Robot.leftRotation(-45.degrees)
+   */
+    //Push lever on stage changer
     Robot.movestraight(50.mm, 245.degreesPerSecond)
     Robot.movestraight(-40.mm, -245.degreesPerSecond)
     Robot.movestraight(50.mm, 245.degreesPerSecond)
@@ -29,23 +35,24 @@ object SceneChangeTrip extends Runnable{
 
   }
 
+//WOPPER RULES
   def startToTv(): Unit = {
     Gyroscope.reset(SensorPort.One)
     Gyroscope.setHeading(SensorPort.One,0.degrees)
     Robot.movestraight(440.mm, 245.degreesPerSecond)
     Robot.movestraight(-30.mm, speed = -200.degreesPerSecond)
   }
-
+  //WOPPER RULES
   def tvToWindmill(): Unit = {
     Robot.leftRotation(-50.degrees)
     Robot.movestraight(330.mm+135.mm, speed = 200.degreesPerSecond)
     Robot.rightRotation(48.degrees)
   }
-
+  //WOPPER RULES
   def eatWindmill(): Unit = {
     Robot.movestraight(300.mm, speed = 400.degreesPerSecond)
     Time.pause(2000.milliseconds)
-
+    //WOPPER RULES
     Robot.movestraight(-50.mm, speed = -400.degreesPerSecond)
     Robot.movestraight(100.mm, speed = 400.degreesPerSecond)
     Time.pause(2000.milliseconds)
@@ -53,7 +60,7 @@ object SceneChangeTrip extends Runnable{
     Robot.movestraight(-50.mm, speed = -400.degreesPerSecond)
     Robot.movestraight(100.mm, speed = 400.degreesPerSecond)
     Time.pause(2000.milliseconds)
-
+    //WOPPER RULES
     Robot.movestraight(-50.mm, speed = -400.degreesPerSecond)
     Robot.movestraight(100.mm, speed = 400.degreesPerSecond)
     Time.pause(2000.milliseconds)
@@ -61,12 +68,18 @@ object SceneChangeTrip extends Runnable{
     Robot.movestraight(-50.mm, speed = -400.degreesPerSecond)
     Robot.movestraight(100.mm, speed = 400.degreesPerSecond)
     Time.pause(2000.milliseconds)
-
+    //WOPPER RULES
     Robot.movestraight(-210.mm, speed = -400.degreesPerSecond)
-  }
-
   def foodForDinoAtHome(): Unit = {
     Robot.leftRotation(-30.degrees)
-    Robot.movestraight(-690.mm, -200.degreesPerSecond)
+    Robot.movestraight(-690.mm, -200.degreesPerSecond)}
   }
-}
+}//Whopper, Whopper, Whopper, Whopper
+  //Junior, Double, Triple Whopper
+    //Flame-grilled taste with perfect toppers
+    //I rule this day
+    //Lettuce, mayo, pickle, ketchup
+  //It's okay if I don't want that
+  //Impossible or bacon Whopper
+    //Any Whopper my way
+
