7c3b0616aa3b1bcd6d8087dc30ec64313faa660f
dwalend <david@walend.net>
2023-10-22 21:41:07 -0400

    5x hello world better

--- Ev3LangScalaExample/src/ev3dev4s/examples/FiveXHelloWorld.scala
+++ Ev3LangScalaExample/src/ev3dev4s/examples/FiveXHelloWorld.scala
@@ -1,17 +1,20 @@
 package ev3dev4s.examples
 
-import ev3dev4s.Log
+import ev3dev4s.{Ev3System, Log}
 import ev3dev4s.lego.{Display, Sound}
 import ev3dev4s.scala2measure.Conversions.IntConversions
 
 //noinspection ScalaUnusedSymbol
 object FiveXHelloWorld extends Runnable {
   override def run(): Unit = {
+    Display.clearLcd()
     Display.setLedsTo(Display.LedColor.Orange)
     Log.log("Hello World")
-    Display.write("Hello World",1)
+    Display.write("Hello World",0)
     Sound.speak("Hello World")
     Sound.playBeep(200.Hz,100.ms)
     Display.setLedsTo(Display.LedColor.Green)
+    Display.write("Push Button",3)
+    Ev3System.keyPad.blockUntilAnyKey()
   }
 }
