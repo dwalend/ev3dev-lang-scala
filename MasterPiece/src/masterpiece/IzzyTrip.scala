57262f768aa56ec538ec063ed3b928ee3c79a08f
dwalend <David@walend.net>
2023-11-03 21:55:53 -0400

    Inna added a new route and pushed the printer slider for 20 points

---
+++ MasterPiece/src/masterpiece/IzzyTrip.scala
@@ -0,0 +1,35 @@
+package masterpiece
+
+import ev3dev4s.lego.Gyroscope
+import ev3dev4s.scala2measure.Conversions.IntConversions
+import ev3dev4s.sensors.SensorPort
+
+import java.lang.Runnable
+import scala.Unit
+
+object IzzyTrip extends Runnable{
+  //start with back of robot on the south wall, right start area
+  override def run(): Unit = {
+    setGyro()
+
+    fromStartToIzzy()
+  }
+
+//WOPPER RULES
+  private def setGyro(): Unit = {
+    //Set the gyroscope
+    Gyroscope.reset(SensorPort.One)
+    Gyroscope.setHeading(SensorPort.One, 0.degrees)
+  }
+
+  private def fromStartToIzzy(): Unit ={
+    //move from start to the scene changer
+    Robot.movestraight(80.mm)
+    Robot.leftRotation(-45.degrees)
+    Robot.movestraight(500.mm)
+
+    Robot.moveStraightBackward(500.mm
+    )
+  }
+
+}
