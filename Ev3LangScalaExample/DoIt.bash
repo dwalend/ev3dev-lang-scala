5b98d77b439c6d5d8e4ba4b09f046bc7c733c9f4
dwalend <david@walend.net>
2024-07-06 17:08:58 -0400

    Arranging code so that bleep can compile it without a lot of fuss.

--- Ev3LangScalaExample/DoIt.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Command line for getting SuperPowered.jar up and running
-java -DlogFile=log.txt -cp Ev3LangScala.jar ev3dev4s.JarRunner Ev3LangScalaExample.jar examples.FiveXHelloWorld
-
-# In a shell use
-# brickrun -r --java -DlogFile=log.txt -cp Ev3LangScala.jar ev3dev4s.JarRunner Ev3LangScalaExample.jar examples.FiveXHelloWorld
