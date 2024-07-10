d5e473e67bb720b4dcab2c7b358a0e982adf68b2
dwalend <david@walend.net>
2024-07-09 22:48:42 -0400

    Making a .jar file!

--- bleepScripts/src/scala/bleep/scripts/Jar.scala
+++ bleepScripts/src/scala/bleep/scripts/Jar.scala
@@ -1,10 +1,34 @@
 package bleep.scripts
 
-import bleep.{BleepScript,Started,Commands}
+import bleep.packaging.{JarType, ManifestCreator, createJar}
+import bleep.{BleepScript, Commands, Started}
+
+import java.io.{BufferedOutputStream, File, FileOutputStream}
+import java.nio.file.{Files, Path}
+import scala.util.Using
 
 object Jar extends BleepScript("Jar") {
   override def run(started: Started, commands: Commands, args: List[String]): Unit = {
-    println("It Worked!")
+    //todo handle variants instead of hardcoding "normal"
+//    println(${started.activeProjectsFromPath})  //todo why does this crap out?
+
+    val jarClassesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(args.head).resolve("classes")
+    //todo resources, too
+    val jarBytes: Array[Byte] = createJar(
+      jarType = JarType.Jar,
+      manifestCreator = ManifestCreator.default,
+      fromFolders = Seq(jarClassesPath),
+      mainClass = Option("ev3dev4s.JarRunner") //todo how to get the main class from the bleep.yaml ?
+    )
+
+    val jarDirectory = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(args.head).resolve("jars")
+    jarDirectory.toFile.mkdirs()
+    val jarPath = jarDirectory.resolve(s"${args.head}.jar")
+    jarPath.toFile.delete()
+    Files.write(jarPath,jarBytes)
+
+    println(s"${jarBytes.length} bytes written to $jarPath")
+
   }
 }
 
