4e4921edfddb7c724a477d7a3ec1840d80cd0bee
dwalend <david@walend.net>
2024-07-12 14:17:39 -0400

    Got scp working, still a little rough

--- bleepScripts/src/scala/bleep/scripts/Jar.scala
+++ bleepScripts/src/scala/bleep/scripts/Jar.scala
@@ -11,12 +11,13 @@ import scala.util.Using
 object Jar extends BleepScript("Jar") {
   override def run(started: Started, commands: Commands, args: List[String]): Unit = {
 
-    commands.compile(List(model.CrossProjectName(ProjectName(args.head),None)))
+    val projectName = ProjectName(args.head)
+    commands.compile(List(model.CrossProjectName(projectName,None)))
     //todo handle variants instead of hardcoding "normal"
 //    println(${started.activeProjectsFromPath})  //todo why does this fail silently, bad exit code?
 
-    val classesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(args.head).resolve("classes")
-    val resourcesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(args.head).resolve("resources") //todo is this the right path for resources?  Do I even need to do it?
+    val classesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(projectName.value).resolve("classes")
+    val resourcesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(projectName.value).resolve("resources") //todo is this the right path for resources?  Do I even need to do it?
     val jarBytes: Array[Byte] = createJar(
       jarType = JarType.Jar,
       manifestCreator = ManifestCreator.default,
@@ -24,14 +25,21 @@ object Jar extends BleepScript("Jar") {
       mainClass = Option("ev3dev4s.JarRunner") //todo how to get the main class from the bleep.yaml ?
     )
 
-    val jarDirectory = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(args.head).resolve("jars")
-    jarDirectory.toFile.mkdirs()
-    val jarPath = jarDirectory.resolve(s"${args.head}.jar")
-    jarPath.toFile.delete()
-    Files.write(jarPath,jarBytes)
+    jarDirectory(started, projectName).toFile.mkdirs()
 
-    println(s"${jarBytes.length} bytes written to $jarPath")
+    jarPath(started, projectName).toFile.delete()
+    Files.write(jarPath(started,projectName),jarBytes)
 
+    println(s"${jarBytes.length} bytes written to ${jarPath(started,projectName)}")
+
+  }
+
+  def jarPath(started: Started,projectName: ProjectName):Path = {
+    jarDirectory(started, projectName).resolve(s"${projectName.value}.jar")
+  }
+
+  def jarDirectory(started: Started,projectName: ProjectName):Path = {
+    started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(projectName.value).resolve("jars")
   }
 }
 
---
+++ bleepScripts/src/scala/bleep/scripts/ScpJar.scala
@@ -0,0 +1,46 @@
+package bleep.scripts
+
+import bleep.model.ProjectName
+import bleep.{BleepScript, Commands, Started}
+
+import java.nio.file.Path
+
+import org.apache.tools.ant.taskdefs.optional.ssh.{Scp,SSHExec}
+import org.apache.tools.ant.{Project => AntProject}
+
+object ScpJar extends BleepScript("ScpJar") {
+  override def run(started: Started, commands: Commands, args: List[String]): Unit = {
+    //todo make the Jar file with something like commands.compile(List(model.CrossProjectName(ProjectName(args.head),None)))
+
+    val projectName = ProjectName(args.head)
+    val jarPath = Jar.jarPath(started,projectName)
+
+    println(s"copy $jarPath")
+    scpJar(jarPath,projectName)
+  }
+
+  /**
+   * Copy a jar file to the ev3 via scp - and write the expected size in another file (in hopes of detecting that the
+   * jar file is complete.)
+   */
+  def scpJar(jarPath: Path,projectName: ProjectName) = {
+    scpFile(jarPath,s"${projectName.value}.jar")
+  }
+
+  def scpFile(fromLocalFile:Path, toRemoteFile:String):Unit = {
+    val ev3UserName = "robot"
+    val ev3Password = "nope"
+    val ev3Hostname = "sally.local"
+
+    val scp = new Scp()
+    scp.init()
+    scp.setProject(new AntProject())
+    scp.setPassword(ev3Password)
+    scp.setLocalFile(fromLocalFile.toString())
+    scp.setRemoteTofile(s"$ev3UserName@$ev3Hostname:$toRemoteFile")
+    scp.setTrust(true)
+    scp.execute()
+    println("\u0007")
+  }
+}
+
