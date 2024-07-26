254813c4991d68e873d36a7a8f3fad67a5a9605a
dwalend <david@walend.net>
2024-07-26 08:07:55 -0400

    Separated building a plain jar from a fat jar

---
+++ bleepScripts/src/scala/bleep/scripts/FatJar.scala
@@ -0,0 +1,67 @@
+package bleep.scripts
+
+import bleep.model.ProjectName
+import bleep.{BleepScript, Commands, Started, model}
+import bloop.config.PlatformFiles
+import org.apache.tools.ant.taskdefs.Manifest.Attribute
+import org.apache.tools.ant.taskdefs.{Jar => AntJar, Manifest => AntManifest}
+import org.apache.tools.ant.types.FileSet
+import org.apache.tools.ant.types.selectors.FilenameSelector
+import org.apache.tools.ant.{Project => AntProject}
+
+import java.nio.file.Path
+
+object FatJar extends BleepScript("FatJar") {
+  override def run(started: Started, commands: Commands, args: List[String]): Unit = {
+
+    val projectName = ProjectName(args.head)
+    val crossProjectName = model.CrossProjectName(projectName,None)
+    commands.compile(List(crossProjectName))
+    //todo handle variants instead of hardcoding "normal"
+    //    println(${started.activeProjectsFromPath})  //todo why does this fail silently, bad exit code?
+
+    val classesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(projectName.value).resolve("classes")
+    val resourcesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(projectName.value).resolve("resources") //todo is this the right path for resources?  Do I even need to do it?
+
+    val destFile = Jar.jarPath(started, projectName).toFile
+    Jar.jarDirectory(started, projectName).toFile.mkdirs()
+    destFile.delete()
+
+    val antProject = new AntProject()
+
+    val antJarTask = new AntJar()
+    antJarTask.init()
+    antJarTask.setProject(antProject)
+
+    antJarTask.setDestFile(destFile)
+
+    val classFiles = new FileSet()
+    classFiles.setDir(classesPath.toFile)
+    antJarTask.addFileset(classFiles)
+
+    if(resourcesPath.toFile.exists()) {
+      val resourceFiles = new FileSet()
+      resourceFiles.setDir(resourcesPath.toFile)
+      antJarTask.addFileset(resourceFiles)
+    }
+
+    //todo find the dependency libraries and add them
+    val classpath: Seq[PlatformFiles.Path] = started.bloopProject(crossProjectName).classpath
+    val libraries = new FileSet()
+    libraries.setProject(antProject)
+    libraries.setDir(classpath.head.toFile.getParentFile)
+    val libraryFileSelector = new FilenameSelector()
+    libraryFileSelector.setName(classpath.head.toFile.getName)
+
+    libraries.add(libraryFileSelector)
+    antJarTask.addZipGroupFileset(libraries)
+
+    val mainClassName = "ev3dev4s.JarRunner$" //todo get this from bleep yaml
+    val antManifest = new AntManifest()
+    antManifest.addConfiguredAttribute(new Attribute("Main-Class",mainClassName))
+    antJarTask.addConfiguredManifest(antManifest)
+
+    antJarTask.execute()
+  }
+}
+
--- bleepScripts/src/scala/bleep/scripts/Jar.scala
+++ bleepScripts/src/scala/bleep/scripts/Jar.scala
@@ -4,70 +4,11 @@ import bleep.model.ProjectName
 import bleep.packaging.{JarType, ManifestCreator, createJar}
 import bleep.{BleepScript, Commands, Started, model}
 import bloop.config.PlatformFiles
-import org.apache.tools.ant.taskdefs.Manifest.Attribute
-import org.apache.tools.ant.taskdefs.{Jar => AntJar, Manifest => AntManifest}
-import org.apache.tools.ant.{Project => AntProject}
-import org.apache.tools.ant.types.{FileSet, ZipFileSet}
-import org.apache.tools.ant.types.selectors.{FileSelector, FilenameSelector}
 
-import java.io.{BufferedOutputStream, File, FileOutputStream}
 import java.nio.file.{Files, Path}
-import scala.util.Using
 
 object Jar extends BleepScript("Jar") {
-  override def run(started: Started, commands: Commands, args: List[String]): Unit = {
-
-    val projectName = ProjectName(args.head)
-    val crossProjectName = model.CrossProjectName(projectName,None)
-    commands.compile(List(crossProjectName))
-    //todo handle variants instead of hardcoding "normal"
-    //    println(${started.activeProjectsFromPath})  //todo why does this fail silently, bad exit code?
-
-    val classesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(projectName.value).resolve("classes")
-    val resourcesPath: Path = started.buildPaths.buildsDir.resolve("normal").resolve(".bloop").resolve(projectName.value).resolve("resources") //todo is this the right path for resources?  Do I even need to do it?
-
-    val destFile = jarPath(started, projectName).toFile
-    jarDirectory(started, projectName).toFile.mkdirs()
-    jarPath(started, projectName).toFile.delete()
-
-    val antProject = new AntProject()
-
-    val antJarTask = new AntJar()
-    antJarTask.init()
-    antJarTask.setProject(antProject)
-
-    antJarTask.setDestFile(destFile)
-
-    val classFiles = new FileSet()
-    classFiles.setDir(classesPath.toFile)
-    antJarTask.addFileset(classFiles)
-
-    if(resourcesPath.toFile.exists()) {
-      val resourceFiles = new FileSet()
-      resourceFiles.setDir(resourcesPath.toFile)
-      antJarTask.addFileset(resourceFiles)
-    }
-
-    //todo find the dependency libraries and add them
-    val classpath: Seq[PlatformFiles.Path] = started.bloopProject(crossProjectName).classpath
-    val libraries = new FileSet()
-    libraries.setProject(antProject)
-    libraries.setDir(classpath.head.toFile.getParentFile)
-    val libraryFileSelector = new FilenameSelector()
-    libraryFileSelector.setName(classpath.head.toFile.getName)
-
-    libraries.add(libraryFileSelector)
-    antJarTask.addZipGroupFileset(libraries)
-
-    val mainClassName = "ev3dev4s.JarRunner$" //todo get this from bleep yaml
-    val antManifest = new AntManifest()
-    antManifest.addConfiguredAttribute(new Attribute("Main-Class",mainClassName))
-    antJarTask.addConfiguredManifest(antManifest)
-
-    antJarTask.execute()
-  }
-
-  def runJustBleep(started: Started, commands: Commands, args: List[String]): Unit = {
+  def run(started: Started, commands: Commands, args: List[String]): Unit = {
 
     val projectName = ProjectName(args.head)
     commands.compile(List(model.CrossProjectName(projectName,None)))
@@ -89,7 +30,6 @@ object Jar extends BleepScript("Jar") {
     Files.write(jarPath(started,projectName),jarBytes)
 
     println(s"${jarBytes.length} bytes written to ${jarPath(started,projectName)}")
-
   }
 
   def jarPath(started: Started,projectName: ProjectName):Path = {
